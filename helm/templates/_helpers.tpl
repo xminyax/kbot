95b88f62acb6909a388f33da02630103454deff0
Ilya Minienkov <xminyax@gmail.com>
2023-05-26 21:49:46 +0300

    add pipeline

--- helm/templates/_helpers.tpl
+++ helm/templates/_helpers.tpl
@@ -50,13 +50,4 @@ app.kubernetes.io/name: {{ include "helm.name" . }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 {{- end }}
 
-{{/*
-Create the name of the service account to use
-*/}}
-{{- define "helm.serviceAccountName" -}}
-{{- if .Values.serviceAccount.create }}
-{{- default (include "helm.fullname" .) .Values.serviceAccount.name }}
-{{- else }}
-{{- default "default" .Values.serviceAccount.name }}
-{{- end }}
-{{- end }}
+
---
+++ pipeline/jenkins.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent any
+    parameters {
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+    }
+    environment {
+        REPO = "https://github.com/minyax/kbot"
+        BRANCH = 'main'
+    }
+    stages {
+        stage('Example') {
+            steps {
+                echo "Build for platform ${params.OS}"
+                echo "Build for arch: ${params.ARCH}"
+            }
+        }
+        stage("clone") {
+            steps {
+                echo 'CLONE REPOSITORY'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+        stage("test") {
+            steps {
+                sh "make --version"
+                echo 'TEST EXECUTION STARTED '
+                sh 'make test'
+            }
+        }
+        stage("build") {
+            steps {
+                echo 'BUILD EXECUTION STARTED '
+                sh 'make build'
+            }
+        }
+        stage("image") {
+            steps {
+                script {
+                    echo 'TEST EXECUTION STARTED '
+                    sh 'make image'
+                }
+            }
+        }
+        stage("push") {
+            steps {
+                script {
+                    docker.withRegistry('', 'dockerhub') {
+                        sh 'make push'
+                    }
+                }
+            }
+        }
+    }
+}
