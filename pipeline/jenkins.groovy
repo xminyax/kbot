ec5a3d8c56e97f2f6258e0bb47ab603a36409a52
Ilya Minienkov <xminyax@gmail.com>
2023-05-27 19:03:56 +0300

    update jenkins.file

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,18 +1,54 @@
 pipeline {
     agent any
     parameters {
-
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
         choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick ARCH')
-
+    }
+    environment {
+        REPO = "https://github.com/xminyax/kbot.git"
+        BRANCH = 'main'
     }
     stages {
         stage('Example') {
             steps {
                 echo "Build for platform ${params.OS}"
-
                 echo "Build for arch: ${params.ARCH}"
-
+            }
+        }
+        stage("clone") {
+            steps {
+                echo 'CLONE REPOSITORY'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+        stage("test") {
+            steps {
+                sh "make --version"
+                echo 'TEST EXECUTION STARTED '
+                sh 'make test'
+            }
+        }
+        stage("build") {
+            steps {
+                echo 'BUILD EXECUTION STARTED '
+                sh 'make build'
+            }
+        }
+        stage("image") {
+            steps {
+                script {
+                    echo 'TEST EXECUTION STARTED '
+                    sh 'make image'
+                }
+            }
+        }
+        stage("push") {
+            steps {
+                script {
+                    docker.withRegistry('', 'dockerhub') {
+                        sh 'make push'
+                    }
+                }
             }
         }
     }
